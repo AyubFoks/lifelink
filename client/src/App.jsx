58f15a7f85c4cd1000a16d301800e7c6ae014828
Patricia <patricianjuguna084@gmail.com>
2025-09-23 20:56:38 +0300

    add App + main router skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/common/Navbar/Navbar";
+import Footer from "./components/common/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import HospitalDashboard from "./pages/Dashboard/HospitalDashboard";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      Hi!
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/:role" element={<Login />} />
+          <Route path="/register/:role" element={<Register />} />
+          <Route path="/dashboard/hospital" element={<HospitalDashboard />} />
+          {/* note to self to add protected routes */}
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
-
-export default App;
