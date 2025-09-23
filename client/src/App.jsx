14a3adb47fb66a99a651561f24f1b830cdee9be5
Patricia <patricianjuguna084@gmail.com>
2025-09-23 13:45:09 +0300

    Add auth context for authentication and wrap main.jsx in the context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div>
+      <NavBar />
       <h1 className="text-3xl">
         LifeLink
       </h1>
+      <Footer />
     </div>
   )
 }
